8a485eda3779161633e38ee528afd7f416e1a962
Angel Lugo Tehuitzil <termopit@hotmail.com>
2024-08-27 16:59:20 -0600

    Correcting format in multiline string.

--- personal-ai-project/helper_functions.py
+++ personal-ai-project/helper_functions.py
@@ -44,20 +44,21 @@ def query_pinecone(query_text, top_k=5):
     )
 
     match_list = []
+    match_string = ""
 
     for matched in results["matches"]:
-        match_string = f"""
-        Chapter: {matched['metadata']['chapter']}
-        Header: {matched['metadata']['title']}
-        Section: {matched['metadata']['section']}
-        Url: {matched['metadata']['url']}
-        --------------------------------------------------
-        Content: {matched['metadata']['content']}
-        """
+        match_string = f"""Chapter: {matched['metadata']['chapter']}
+Header: {matched['metadata']['title']}
+Section: {matched['metadata']['section']}
+Url: {matched['metadata']['url']}
+--------------------------------------------------
+Content: {matched['metadata']['content']}
+"""
 
         match_list.append(match_string)
-        match_string = "\n".join(match_list)
 
+    match_string = "\n".join(match_list)
+    
     return match_string
 
 def format_chat_prompt(message, chat_history):
@@ -70,29 +71,25 @@ def format_chat_prompt(message, chat_history):
 
 def respond(message, chat_history):
         
-        information = query_pinecone(message)
-        information = "\n".join(information)
+        information = query_pinecone(message)        
 
-        full_prompt = f"""
-        Your role:
-        You are a chatbot that answers questions about the General Handbook of the Church of Jesus Christ of Latter Day Saints.
+        full_prompt = f"""Your role:
+You are a chatbot that answers questions about the General Handbook of the Church of Jesus Christ of Latter Day Saints.
 
-        Intructions:
-        Below is a list of information received from a vector database where you might find information to answer the user's question about the General Handbook.
-        Each chunk of information contains a title, url, and content. You will mainly answer questions from the content, but feel free to share that extra information as reference.
+Intructions:
+Below is a list of information received from a vector database where you might find information to answer the user's question about the General Handbook.
+Each chunk of information contains a title, url, and content. You will mainly answer questions from the content, but feel free to share that extra information as reference.
 
-        Information:
-        START OF INFORMATION
+Information:
+START OF INFORMATION
 
-        {information}
+{information}
+END OF INFORMATION
 
-        END OF INFORMATION
-
-        (Further instructions: If the user seems to be asking about something not related to the General Handbook, invite them to ask about it. If instead of a questions they seem to be thanking you for previous answers, show that you welcome their gratitude)
-        User Input:
-        {message}
-        """
-
+(Further instructions: If the user seems to be asking about something not related to the General Handbook, invite them to ask about it. If instead of a questions they seem to be thanking you for previous answers, show that you welcome their gratitude)
+User Input:
+{message}
+"""
 
         formatted_prompt = format_chat_prompt(full_prompt, chat_history)
         bot_message = text_generator.generate_content(formatted_prompt).text
